b30eaf44ba93c090886f823ae45a053640cb1a2a
Rohan Kumar <rohan.shuvam@gmail.com>
2024-07-02 21:54:40 +0530

    updated pino and added global logger

--- src/index.js
+++ src/index.js
@@ -28,6 +28,14 @@ import admin from "./routes/admin/index.js";
 
     app.use(Express.json());
 
+    app.use((err, req, res, next) => {
+        logger.error(err);
+        res.status(500).json({
+            message:
+                "Something went wrong, Please check request or contact system admin!!",
+        });
+    });
+
     app.use("/v1", version_1);
 
     app.use(
